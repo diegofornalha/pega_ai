71658aca98c6bdc0e4f93750bf5483a5fb53b76f
Diego Fornalha <diegofornalha@gmail.com>
2023-09-28 09:30:11 -0300

    botoes responsivos

--- src/ui/components/Landing.js
+++ src/ui/components/Landing.js
@@ -14,9 +14,9 @@ export default function Landing(props) {
   const [transactionInProgress] = useRecoilState(transactionInProgressState)
 
   return (
-    <div className="flex flex-col gap-y-20">
-      <div className="mt-10 flex gap-y-5 sm:gap-x-5 flex-col-reverse sm:flex-row justify-between items-center">
-        <div className="px-2 flex flex-col gap-y-8 items-start">
+    <div className="flex flex-col gap-y-10 sm:gap-y-20">
+      <div className="mt-5 sm:mt-10 flex gap-y-4 sm:gap-y-5 flex-col-reverse sm:flex-row justify-between items-center">
+        <div className="px-2 flex flex-col gap-y-6 sm:gap-y-8 items-start">
           <div className="flex flex-col gap-y-2">
             <div className={styles.landing}>
               <div className={styles.ltitle}>
@@ -24,7 +24,6 @@ export default function Landing(props) {
                 <div className="underline text-black font-bold decoration-drizzle-green decoration-4">Drops</div>
               </div>
             </div>
-
             <label className={`-mt-1 font-flow text-black font-bold text-5xl sm:text-6xl`}>nunca foi</label>
             <label className={`font-flow text-black font-bold text-5xl sm:text-6xl`}>tão simples!</label>
           </div>
@@ -32,11 +31,11 @@ export default function Landing(props) {
             <label className={`-mt-5 font-flow text-gray-400 font-medium text-md`}>
               Crie um Sorteio em um minuto!
             </label>
-            <a href="https://www.notion.so/lanford33/drizzle-d4713d571a944361a5fc80a915e01fc7"
+            <a href="https://www.twitter.com/flowbrasilchain"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <label className={`cursor-pointer font-flow text-drizzle-green font-medium text-md underline decoration-drizzle-green decoration-3`}>
+            <label className={`cursor-pointer font-flow text-drizzle-green font-medium text-md underline decoration-drizzle-green decoration-3`}>
               </label>
             </a>
           </div>
@@ -44,7 +43,8 @@ export default function Landing(props) {
           {props.user && props.user.loggedIn ?
             <div className="-mt-5 flex flex-col gap-y-2">
               <label className="font-flow text-gray-400 font-medium text-md">Escolha uma opção no Menu abaixo:</label>
-              <div className="flex gap-x-2">
+              <div className="flex flex-wrap gap-x-2 gap-y-2">
+               
                 <button
                   type="button"
                   disabled={transactionInProgress}
@@ -113,7 +113,7 @@ export default function Landing(props) {
           }
         </div>
 
-        <div className="w-[350px] h-[350px] sm:w-[420px] sm:h-[420px] relative">
+        <div className="w-[250px] h-[250px] sm:w-[350px] sm:h-[350px] md:w-[420px] md:h-[420px] relative">
           <Image src="/landing.png" alt="" layout="responsive" width={400} height={400} objectFit="cover" priority={true} />
         </div>
       </div>
