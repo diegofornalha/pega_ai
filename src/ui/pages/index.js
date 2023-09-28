c0b07269526d06a200be0fcdc837486ac00753ac
Diego Fornalha <diegofornalha@gmail.com>
2023-09-28 09:16:14 -0300

    ajustando para mobile

--- src/ui/pages/index.js
+++ src/ui/pages/index.js
@@ -3,9 +3,9 @@ import Landing from '../components/Landing'
 export default function Home(props) {
   return (
     <>
-    <div className="container mx-auto max-w-[920px] min-w-[380px] px-6">
-      <Landing user={props.user} auth={props.auth}/>
-    </div>
+      <div className="container mx-auto max-w-[920px] px-6 sm:px-8 md:px-12">
+        <Landing user={props.user} auth={props.auth}/>
+      </div>
     </>
   )
 }
