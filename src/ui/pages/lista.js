89ceffe30b0f955a98ac211da934c14f182296d9
Diego Fornalha <diegofornalha@gmail.com>
2023-10-02 23:25:31 -0300

    acabei de adicionar mais uma pagina chamada lista

---
+++ src/ui/pages/lista.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Lista = () => {
+    return (
+        <div>
+            <h1>Lista</h1>
+            <p>Esta é a página da lista.</p>
+        </div>
+    );
+}
+
+export default Lista;
