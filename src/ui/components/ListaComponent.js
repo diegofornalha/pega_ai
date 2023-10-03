300e306bb7d7dcbd6c3d13f046581315d061badc
Diego Fornalha <diegofornalha@gmail.com>
2023-10-03 01:34:34 -0300

    embedado

--- src/ui/components/ListaComponent.js
+++ src/ui/components/ListaComponent.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import styles from '../styles/ListaComponent.module.css';
 
 const ListaComponent = (props) => {
-  return (
-    <div>
-      Conteúdo da página Lista.
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <iframe 
+                src="https://float.diegofornalha.repl.co/" 
+                width="100%" 
+                height="500px" 
+                frameBorder="0" 
+                title="Formulário Embedado"
+            ></iframe>
+        </div>
+    );
 }
 
 export default ListaComponent;
