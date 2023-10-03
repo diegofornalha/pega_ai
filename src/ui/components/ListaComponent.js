6a50b357326e39448df5afe4147e54c92b6d1de5
Diego Fornalha <diegofornalha@gmail.com>
2023-10-03 01:03:16 -0300

    embedei, e funcionou

--- src/ui/components/ListaComponent.js
+++ src/ui/components/ListaComponent.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styles from '../styles/ListaComponent.module.css';
 
 const ListaComponent = (props) => {
-  return (
-    <div>
-      Conteúdo da página Lista.
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <iframe 
+                src="https://float.diegofornalha.repl.co/" 
+                width="100%" 
+                height="500px" 
+                frameborder="0" 
+                allowfullscreen 
+                title="Formulário Embedado"
+            ></iframe>
+        </div>
+    );
 }
 
 export default ListaComponent;
