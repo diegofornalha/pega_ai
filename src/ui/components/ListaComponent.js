e4193eb5047a5fbe00f2be1688232a8d0481588c
Diego Fornalha <37958057+diegofornalha@users.noreply.github.com>
2024-01-09 16:42:12 -0300

    Update ListaComponent.js

--- src/ui/components/ListaComponent.js
+++ src/ui/components/ListaComponent.js
@@ -5,7 +5,7 @@ const ListaComponent = (props) => {
     return (
         <div className={styles.container}>
             <iframe 
-                src="https://float.diegofornalha.repl.co/" 
+                src="https://8cc47abe-fd2b-4835-8918-3b6541ba5644-00-1fyrt48fgyf2d.global.replit.dev/" 
                 width="100%" 
                 height="500px" 
                 frameBorder="0" 
